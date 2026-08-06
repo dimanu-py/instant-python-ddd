684080c8663f47639a652abbc233fde6715b83e7
dimanu-py <dimanu.py@gmail.com>
2026-08-06 07:58:38 +0200

    refactor(config): rename inner method in git step question wizard to detect if user wants to create the project in a git repository

--- instant_python/config/infra/question_wizard/step/git_step.py
+++ instant_python/config/infra/question_wizard/step/git_step.py
@@ -14,7 +14,7 @@ class GitStep(Step):
         return "Git"
 
     def run(self) -> dict[str, dict[str, str | bool]]:
-        if not self._user_wants_initialize_git_repository():
+        if self._git_repository_initialization_is_skipped():
             return {self._KEY: self._answers}
 
         self._ask_git_username()
@@ -34,10 +34,10 @@ class GitStep(Step):
         )
         self._answers["username"] = answer
 
-    def _user_wants_initialize_git_repository(self) -> bool:
+    def _git_repository_initialization_is_skipped(self) -> bool:
         answer = self._questionary.boolean_question(
             message="Do you want to initialize a git repository?",
             default=True,
         )
         self._answers["initialize"] = answer
-        return answer
+        return not answer
